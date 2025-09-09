383d783a2bea3ebcef6ad8afaa9f00c04ec1837a
Christian Parkinson <christianparkinson@b98-aruba-authenticated-10-110-178-184.wlan.cpp.edu>
2025-09-09 16:52:59 -0700

    Completed Assignment

---
+++ Assignment 1: Hello World + IDE & Accessibility/HelloWorld.java
@@ -0,0 +1,10 @@
+// Christian O. Parkinson
+// CS1400
+// Assignment 1
+// 2025-09-08
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
