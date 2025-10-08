c2130ea82df20b242241d61e6e173f076c6cafbb
Christian Parkinson <christianparkinson@b98-aruba-authenticated-10-110-180-135.wlan.cpp.edu>
2025-10-07 23:23:19 -0700

    CS 1400/Assignment 3: Completed CheeringCrowd program

--- CS 1400/Assignment 3: Line Charts, File Input, Loops, and Switch Statements/CheeringCrowd.java
+++ CS 1400/Assignment 3: Line Charts, File Input, Loops, and Switch Statements/CheeringCrowd.java
@@ -4,3 +4,16 @@ Course: CS 1400
 Assignment: 3
 Task: C
 */
+
+class CheeringCrowd {
+    public static void main(String[] args) {
+        int crowdSize = 5;
+        
+        for (int i = crowdSize; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("\\o/ ");
+            }
+            System.out.println();
+        }
+    }
+}
