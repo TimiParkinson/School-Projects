a3a15de713b6de89678c3501082d4c8d146e10e2
Christian Parkinson <christianparkinson@b98-aruba-authenticated-10-110-180-135.wlan.cpp.edu>
2025-10-07 23:30:05 -0700

    CS 1400/Assignment 3: Completed SwitchMessage program

--- CS 1400/Assignment 3: Line Charts, File Input, Loops, and Switch Statements/SwitchMessage.java
+++ CS 1400/Assignment 3: Line Charts, File Input, Loops, and Switch Statements/SwitchMessage.java
@@ -4,3 +4,14 @@ Course: CS 1400
 Assignment: 3
 Task: D
 */
+
+class SwitchMessage {
+    public static void main(String[] args) {
+        int position = 2; // Example position, can be changed to test different outputs
+        switch (position) {
+            case 1 -> System.out.println("You won!");
+            case 2, 3 -> System.out.println("You were so close!");
+            default -> System.out.println("Time to train harder!");  
+        }
+    }
+}
